64e358be74b3fbaba7490c6926b733b72acdb0ee
kapil <amitsaini8385@gmail.com>
2025-12-07 12:06:56 +0530

    git ignore update

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/k/groniq-web/app/layout\":[\"static/media/ee40bb094c99a29a-s.p.woff2\",\"static/media/eafabf029ad39a43-s.p.woff2\",\"static/media/8888a3826f4a3af4-s.p.woff2\",\"static/media/0484562807a97172-s.p.woff2\",\"static/media/b957ea75a84b6ea7-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
